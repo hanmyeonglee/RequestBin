1e5e2e71cca327f495b682d43142cd75b68e9b7a
Jongwon <ejongwon7@gmail.com>
2026-03-03 22:17:54 +0900

    Feat: implement db initializing code

--- api/requestbin/src/main/scala/Main.scala
+++ api/requestbin/src/main/scala/Main.scala
@@ -1,12 +1,9 @@
-import scalikejdbc.config._
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler
 import org.eclipse.jetty.ee10.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
 @main def main(): Unit = {
-  DBs.setupAll()
-  
   val port = sys.env.getOrElse("PORT", "80").toInt
   val server = new Server(port)
   val context = new ServletContextHandler()
--- api/requestbin/src/main/scala/ScalatraBootstrap.scala
+++ api/requestbin/src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,14 @@
 import org.scalatra.LifeCycle
 import jakarta.servlet.ServletContext
 import interface.RequestBinServlet
+import scalikejdbc.config._
+import config.InitDatabase
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext): Unit = {
     context.mount(new RequestBinServlet, "/*")
+
+    DBs.setupAll()
+    InitDatabase.init()
   }
 }
